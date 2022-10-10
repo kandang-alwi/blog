f4181f7baef06005f45a9a92c5d5861e248569aa
Dev_Dio <77707953+dio2282@users.noreply.github.com>
2022-10-10 20:06:57 +0700

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,15 +3,15 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  title: 'Kandang Alwi',
+  subtitle: 'Kandang Alwi Official Blog',
   lang: 'en-US',
-  description: 'Powered by SvelteKit/Urara',
+  description: 'Powered by Kandang Alwi',
   author: {
-    name: 'John Doe',
+    name: 'Kandang Alwi',
     avatar: '/assets/maskable@192.png',
     status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    bio: 'Blog Account.'
   },
   themeColor: '#3D4451'
 }
