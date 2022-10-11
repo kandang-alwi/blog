e3caa3c2c389562cda7659a2bd2850ee3ca283da
Dev_Dio <77707953+dio2282@users.noreply.github.com>
2022-10-11 18:33:20 +0700

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'https://discord.gg/XFVFYgYW',
   title: 'Kandang Alwi',
   subtitle: 'Kandang Alwi Official Blog',
   lang: 'en-US',
