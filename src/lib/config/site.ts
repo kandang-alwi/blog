fdc23233cc63a1ddfef09e58fdc0e71416bae131
Dev_Dio <77707953+dio2282@users.noreply.github.com>
2023-07-06 19:07:54 +0700

    Update

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'blog.kandangalwi.me',
   title: 'Kandang Alwi',
   subtitle: 'Sweet & Powerful SvelteKit Blog Template',
   lang: 'en-US',
